2bda20a463adad103b8b9c61c2aca7e04833618e
nathaliacandioto <nathaliacandioto@gmail.com>
2026-04-27 19:25:18 -0300

    commit initial

---
+++ App.js
@@ -0,0 +1,14 @@
+import react from 'react';
+import {View,Text} from 'react-native'
+
+import Homepage from './src/components/pages/homepage'
+
+const App = () =>{
+
+  return (
+    <Homepage/>
+  );
+
+}
+
+export default App;
---
+++ src/components/pages/homepage.js
@@ -0,0 +1,15 @@
+import react from 'react';
+import {View,Text} from 'react-native'
+
+const Homepage = () =>{
+  return (
+    <View style={{flex:1, alignItems:'center', justifyContent:'center'}}> 
+
+    <Text> Puc Minas </Text>
+    
+    </View>
+  );
+
+}
+
+export default Homepage;
